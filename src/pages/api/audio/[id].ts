d64be14ecbff3db1b34888ba6026512cfd9614ef
Luiz Otávio L. Falcão <39093175+llfalcao@users.noreply.github.com>
2023-05-30 00:15:04 -0300

    feat(api): get highest quality audio

--- src/pages/api/audio/[id].ts
+++ src/pages/api/audio/[id].ts
@@ -6,13 +6,19 @@ const fetchTrack = async (yid: string) => {
   const url = `https://www.youtube.com/watch?v=${yid}`;
   try {
     const { formats } = await ytdl.getInfo(url);
-    const format = ytdl.chooseFormat(formats, { filter: "audioonly" });
+    const format = ytdl.chooseFormat(formats, {
+      filter: "audioonly",
+      quality: "highestaudio",
+    });
 
     return format?.url;
   } catch (error) {
     console.error("# ytdl-core failed, using @distube/ytdl-core", error);
     const { formats } = await distubeYtdl.getInfo(url);
-    const format = distubeYtdl.chooseFormat(formats, { filter: "audioonly" });
+    const format = distubeYtdl.chooseFormat(formats, {
+      filter: "audioonly",
+      quality: "highestaudio",
+    });
 
     return format?.url;
   }
